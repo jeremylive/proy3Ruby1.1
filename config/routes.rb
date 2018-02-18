9f5127fd72f595ebd4c8f2f19e517b56d02ebeca
codelive <jlive@ic-itcr.ac.cr>
2018-02-18 10:46:18 -0600

    avance#1.1.4 final 100%

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  	
 
+  devise_for :users
   get "index/home"
 
   root 'index#home'
 
-  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
