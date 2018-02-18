1374748a4d29197873e9c91a2d1f27bf75baae8f
codelive <jlive@ic-itcr.ac.cr>
2018-02-17 22:18:19 -0600

    avance#1.1
    :100:

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180217221440) do
+ActiveRecord::Schema.define(version: 20180218040628) do
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "email", default: "", null: false
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20180217221440) do
     t.datetime "last_sign_in_at"
     t.string "current_sign_in_ip"
     t.string "last_sign_in_ip"
+    t.string "name"
+    t.integer "permission"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
